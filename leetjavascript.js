f6780b0c1a20b43117123cccfe88dc300efb87c3
Grady L Shelton <gradyish@gmail.com>
2017-10-24 23:05:05 -0700

    add an html page to easily debug in chrome. add two different palindrome examples

--- leetjavascript.js
+++ leetjavascript.js
@@ -35,17 +35,62 @@
 // };
 // console.log(reverse(-900000));
 
-var reverse = function(x) {
-  var answer = 0;
-  while(x > 1){
-    var tail = x % 10;
-    var newAnswer = answer * 10 + tail;
-    // if ((newAnswer - tail) / 10 != answer){
-    //   return 0;
-    // };
-    answer = newAnswer;
-    x /= 10;
-  }
-  return answer;
-}
-console.log(reverse(123));
+// reverse the integer
+
+// var reverse = function(x) {
+//   var answer = '';
+//   var newArray = [];
+//   var array = (x.toString()).split('');
+//
+//   while(array.length>1){
+//
+//     newArray.push(array.pop());
+//     if (array.length == 1 && array[0] == '-'){
+//       newArray.unshift(array[0]);
+//     };
+//   };
+//
+//   answer = parseInt(newArray.join(''));
+//   if(Math.abs(answer) > 2**53 - 1){
+//     return 0;
+//   } else {
+//     return answer;
+//   };
+// };
+// console.log(reverse(-900000));
+// console.log(reverse(123));
+
+// var isPalindrome = function(x) {
+//   var array = (x.toString()).split('');
+//   var reverseArray = array.reverse();
+//   if (x == reverseArray.join('')) {
+//     return true;
+//   } else {
+//     return false;
+//   };
+// };
+//
+// console.log(isPalindrome(123));
+// console.log(isPalindrome(14341));
+
+var efficientPalindrome = function(x) {
+  var array = (x.toString()).split('');
+  var length = array.length;
+  var halved = Math.floor(length/2);
+  for (i=0; i<= halved - 1 ; i++) {
+    var endIndex = length - 1 - i;
+    console.log(array[i] + array[endIndex]);
+    if (array[i] === array[(endIndex)]){
+      var palindrome = true;
+    } else {
+      var palindrome = false;
+    };
+  };
+  return palindrome;
+};
+
+$(document).ready(function(){
+  $("#submit").click(function() {
+    $('#answer').append("answer: " + efficientPalindrome(12321));
+  });
+});
