40dd5d5cd5dc12e7df24d50d1b451f63ccaf9b3a
Grady Shelton <gradyish@gmail.com>
2019-08-16 23:56:44 -0700

    easy single number problem

--- leetjavascript.js
+++ leetjavascript.js
@@ -348,9 +348,21 @@ var romanToInt = function (romanNumerals) {
         continue;
     }
   }
-
   return total;
 }
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  nums.sort()
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1] && nums[i] !== nums[i - 1]) {
+      return nums[i];
+    }
+  }
+};
 
 $(document).ready(function(){
   $("#submit").click(function() {
